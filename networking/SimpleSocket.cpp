2b3090fe45fcc316b3a1de6d3f9fc22e4900f6e7
Yagyesh <saraswatthrust@gmail.com>
2026-03-04 06:26:43 +0530

    Finish with binding

--- networking/SimpleSocket.cpp
+++ networking/SimpleSocket.cpp
@@ -1,6 +1,43 @@
 #include"SimpleSocket.hpp"
 
-SOCKET::SimpleSocket::SimpleSocket(int doimain, int service, int protocol)
+// 04/03/26 - HDE in the video, is SOCKET here
+
+SOCKET::SimpleSocket::SimpleSocket(int doimain, int service, int protocol, int port, u_long interface)
 {
-    int connection = socket(doimain, service, protocol);
+    // Define adress structure
+    adress.sin_family = domain;
+    addess.sin_port = htons(port);
+    addess.sin_addr.s_addr = htonl(interface);
+
+    // Establish a socket
+    sock = socket(doimain, service, protocol);
+    test_connection(sock);
+
+    // Establish network connection
+    connection = connect_to_network(sock, address);
+    test_connection(connection);
+}
+
+void SOCKET::SimpleSocket::test_connection(int item_to_test)
+{
+    if (item_to_test < 0)
+    {
+        errir("Failed to connect....");
+        exit(EXIT_FAILURE);
+    }
+}
+
+struct sockadddr_in SOCKET::SimpleSocket::get_address()
+{
+    return address;
+}
+
+int SOCKET::SimpleSocket::get_sock()
+{
+    return sock;
+}
+
+int SOCKET::SimpleSocket::get_connection()
+{
+    return connection;
 }
--- networking/SimpleSocket.hpp
+++ networking/SimpleSocket.hpp
@@ -4,13 +4,28 @@
 
 #include<stdio.h>
 #include<sys/socket.h>
+#include<netinet/in.h>
+#include<iostream>
 
 namespace SOCKET
 {
     class SimpleSocket 
     {
+        private: 
+            struct socaddr_in address;
+            int sock;
+            int connection;
+
         public: 
-            SimpleSocket(int domain, int service, int protocol);
+            
+            SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
+            virtual int connection_to_network(int sock, struct sockaddr_in address) = 0;
+            void test_connection(int);
+
+            // Getters
+            struct sockaddr_in get_address();
+            int get_sock();
+            int get_connection();
     };
 }
 
